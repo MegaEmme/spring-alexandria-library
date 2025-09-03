b736f8e9993bd1558d421e4e91c3dda9cb2e034d
MegaEmme <mikmusco@gmail.com>
2025-09-03 15:18:03 +0200

    feat: categories create index and show

--- src/main/java/org/lessons/java/spring_alexandria_library/controller/CategoryController.java
+++ src/main/java/org/lessons/java/spring_alexandria_library/controller/CategoryController.java
@@ -1,10 +1,18 @@
 package org.lessons.java.spring_alexandria_library.controller;
 
+import org.lessons.java.spring_alexandria_library.model.Category;
 import org.lessons.java.spring_alexandria_library.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/categories")
@@ -13,8 +21,35 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    // INDEX
+    @GetMapping
     public String index(Model model) {
-        model.addAttribute("category", categoryRepository.findAll());
+        model.addAttribute("categories", categoryRepository.findAll());
         return "categories/index";
+    }
+
+    // SHOW
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer Id, Model model) {
+        model.addAttribute("category", categoryRepository.findById(Id).get());
+        return "categories/show";
+    }
+
+    // CREATE
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("category", new Category());
+        return "categories/create-or-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("category") Category formCategory, BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "categories/create-or-edit";
+        }
+
+        categoryRepository.save(formCategory);
+        return "redirect:/categories";
     }
 }
